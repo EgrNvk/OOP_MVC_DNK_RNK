7787539823b7dffd6fe12395e72e4591774d7549
EgrNvk <104683701+EgrNvk@users.noreply.github.com>
2025-11-05 10:14:20 +0200

    Create view

---
+++ OOP_MVC_DNK_RNK_view.py
@@ -0,0 +1,20 @@
+class ConsoleView:
+    def print_menu(self):
+        print("===DNA/RNA MVC console===")
+        print("1. Ввести/оновити послідовність")
+        print("2. Перевірити валідність")
+        print("3. Транскрибувати ДНК -> РНК")
+        print("4. Зворотна транскрипція РНК -> ДНК")
+        print("5. Комплементарна послідовність")
+        print("6. Обчислити GC%")
+        print("7. Мутація")
+        print("0. Вихід")
+
+    def prompt(self, msg):
+        return input(msg)
+
+    def show(self, msg):
+        print(msg)
+
+    def show_sequence(self, model):
+        print("SEQ:", model.seq)
