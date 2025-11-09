f5726355632159e0518b29747d88da7200a24345
EgrNvk <104683701+EgrNvk@users.noreply.github.com>
2025-11-09 20:01:24 +0200

    Changed view with colorama

--- OOP_MVC_DNK_RNK_view.py
+++ OOP_MVC_DNK_RNK_view.py
@@ -1,22 +1,25 @@
+from colorama import Fore, init
+init(autoreset=True)
+
 class ConsoleView:
     def print_menu(self):
-        print("===DNA/RNA MVC console===")
-        print("1. Ввести/оновити послідовність")
-        print("2. Перевірити валідність")
-        print("3. Транскрибувати ДНК -> РНК")
-        print("4. Зворотна транскрипція РНК -> ДНК")
-        print("5. Комплементарна послідовність")
-        print("6. Обчислити GC%")
-        print("7. Мутація")
-        print("8. Зберегти у файл (FASTA)")
-        print("9. Завантажити з файлу (FASTA)")
-        print("0. Вихід")
+        print(Fore.CYAN + "===DNA/RNA MVC console===")
+        print(Fore.YELLOW + "1. Ввести/оновити послідовність")
+        print(Fore.YELLOW + "2. Перевірити валідність")
+        print(Fore.YELLOW + "3. Транскрибувати ДНК -> РНК")
+        print(Fore.YELLOW + "4. Зворотна транскрипція РНК -> ДНК")
+        print(Fore.YELLOW + "5. Комплементарна послідовність")
+        print(Fore.YELLOW + "6. Обчислити GC%")
+        print(Fore.YELLOW + "7. Мутація")
+        print(Fore.YELLOW + "8. Зберегти у файл (FASTA)")
+        print(Fore.YELLOW + "9. Завантажити з файлу (FASTA)")
+        print(Fore.RED + "0. Вихід")
 
     def prompt(self, msg):
-        return input(msg)
+        return input(Fore.GREEN + msg)
 
     def show(self, msg):
-        print(msg)
+        print(Fore.CYAN + msg)
 
     def show_sequence(self, model):
-        print("SEQ:", model.seq)
+        print(Fore.MAGENTA + "SEQ:", model.seq)
